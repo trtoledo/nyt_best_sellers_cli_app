27852fade306e28c8ee4c8ce96bc5e4b86ff105f
Tomas Toledo <trtoledo1977@gmail.com>
2018-04-16 22:43:48 +0000

    Done.

--- lib/nyt_best_sellers_cli_app/book.rb
+++ lib/nyt_best_sellers_cli_app/book.rb
@@ -1,7 +1,16 @@
-class NytBestSellersCliApp::Books
+class NytBestSellersCliApp::Book
+  attr_accessor :name, :author, :price, :synopsis
 
   def self.today
 
   end
+
+  book_1_f = self.new
+  book_1_f.name = "Book 1 f"
+  book_1_f.author = "Book 1 f Author"
+  book_1_f.price = "Book 1 f Price"
+  book_1_f.synopsis = "Book 1 f Synopsis"
+
+  [Books...]
 
 end
