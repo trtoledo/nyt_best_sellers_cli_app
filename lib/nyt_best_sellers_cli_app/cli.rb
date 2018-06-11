8b5d3f11bcbc3e594be9cf39e9ad32b0cb88d772
Tomas Toledo <trtoledo1977@gmail.com>
2018-06-11 14:05:11 +0000

    Improving interface

--- lib/nyt_best_sellers_cli_app/cli.rb
+++ lib/nyt_best_sellers_cli_app/cli.rb
@@ -38,8 +38,8 @@ class NytBestSellersCliApp::CLI
     #binding.pry
     @fiction_books.each.with_index(1) { |book, i|
       puts "#{i}. #{book.name} - #{book.author}"}
-      puts "If you want information about any book type it`s number!"
-      puts "If you want choose an other list type lists and to leave type exit"
+      puts "If you want more information about any book in this list type it`s number!"
+      puts "If you want to choose an other list type 'lists' and to leave type 'exit'"
 
       while @input != "exit"
         @input = gets.strip.downcase
@@ -47,11 +47,15 @@ class NytBestSellersCliApp::CLI
           the_book = @fiction_books[@input.to_i - 1]
           puts "#{@input}. #{the_book.name} - #{the_book.author}  -  #{the_book.publisher}"
           puts "#{the_book.synopsis}"
-          puts "If you want choose an other book type it`s number, if you want an other list type 'lists' or type type 'exit' to leave"
-        elsif @input == "lists" || @input == "exit"
+          puts "If you want choose an other book of this list type it`s number, if you want an other list type 'lists' or type type 'exit' to leave"
+        elsif @input == "lists"
+          which_list
+        elsif @input == "exit"
           break
         else
-          puts "Sorry didn`t undestand type lists or exit."
+          puts "Sorry didn`t undestand."
+          puts "If you want more information about any book in this list type it`s number!"
+          puts "If you want to choose an other list type 'lists' and to leave type 'exit'"
         end
       end
     end
@@ -60,23 +64,27 @@ class NytBestSellersCliApp::CLI
     @nonfiction_books = NytBestSellersCliApp::Book.scrape_nonfiction_books
     @nonfiction_books.each.with_index(1) { |book, i|
       puts "#{i}. #{book.name} - #{book.author}"}
-      puts "If you want information about any book type it`s number!"
-      puts "If you want choose an other list type lists and to leave type exit"
+      puts "If you want more information about any book in this list type it`s number!"
+      puts "If you want to choose an other list type 'lists' and to leave type 'exit'"
 
-    while @input != "exit"
-      @input = gets.strip.downcase
-      if @input.to_i > 0
-        the_book = @nonfiction_books[@input.to_i - 1]
-        puts "#{@input}. #{the_book.name} - #{the_book.author}  -  #{the_book.publisher}"
-        puts "#{the_book.synopsis}"
-        puts "If you want choose an other list type it`s number or type exit to leave."
-      elsif @input == "lists" || @input == "exit"
-        break
-      else
-        puts "Sorry didn`t undestand type lists or exit."
+      while @input != "exit"
+        @input = gets.strip.downcase
+        if @input.to_i > 0
+          the_book = @nonfiction_books[@input.to_i - 1]
+          puts "#{@input}. #{the_book.name} - #{the_book.author}  -  #{the_book.publisher}"
+          puts "#{the_book.synopsis}"
+          puts "If you want choose an other book of this list type it`s number, if you want an other list type 'lists' or type type 'exit' to leave"
+        elsif @input == "lists"
+          which_list
+        elsif @input == "exit"
+          break
+        else
+          puts "Sorry didn`t undestand."
+          puts "If you want more information about any book in this list type it`s number!"
+          puts "If you want to choose an other list type 'lists' and to leave type 'exit'"
+        end
       end
     end
-  end
 
   def goodbye
     puts "See you soon and good reading!"
