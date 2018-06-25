38eadf2edd12e5df00aff0877794dc905d249b9d
Tomas Toledo <trtoledo1977@gmail.com>
2018-06-25 15:14:13 +0000

    refactoring after assessment

--- lib/nyt_best_sellers_cli_app/book.rb
+++ lib/nyt_best_sellers_cli_app/book.rb
@@ -1,5 +1,9 @@
 class NytBestSellersCliApp::Book
   attr_accessor :name, :author, :publisher, :synopsis, :type
+  #1. Make a Scraper class that scrapes
+  #2. Build a finder method self.find_by_typefiection)
+  #3. Prevent scraping the same site more than once
+  #4. Fix the error on number that's too big
 
    @@all = []
 
@@ -18,7 +22,8 @@ class NytBestSellersCliApp::Book
   end
 
   def self.scrape_fiction_books
-    doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction/"))
+    # doc = Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction/"))
+    doc = scrape_fiction
     books = doc.css (".book-body")
     books.collect { |book|
       name = book.search("h2.title").text
@@ -27,6 +32,11 @@ class NytBestSellersCliApp::Book
       synopsis = book.search("p.description").text
       type = "Fiction"
       self.new(name, author, publisher, synopsis, type)}
+  end
+
+  def self.scrape_fiction
+    puts "SCRAPING FICTION *********"
+    Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction/"))
   end
 
   def self.scrape_nonfiction_books
